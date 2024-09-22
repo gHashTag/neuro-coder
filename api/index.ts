0f00625cf4da2b059c8c75dea37feefe69229b05
Dmitriy Vasilev <raoffonom@icloud.com>
2024-09-22 11:54:32 +0700

    ts

--- api/index.ts
+++ api/index.ts
@@ -5,9 +5,9 @@ import bot from "../src/core/bot";
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
+function runMiddleware(req: any, res: any, fn: any) {
     return new Promise((resolve, reject) => {
-        fn(req, res, (result) => {
+        fn(req, res, (result: any) => {
             if (result instanceof Error) {
                 return reject(result);
             }
