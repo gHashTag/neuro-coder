240302ef3f6bad0bf52a61379d467ee659bc5cae
Dmitriy Vasilev <raoffonom@icloud.com>
2024-09-22 12:29:06 +0700

    deploy

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,10 @@
-require("../src/index");
-
 import { webhookCallback } from "grammy";
 import bot from "../src/core/bot";
+import { IncomingMessage, ServerResponse } from "http";
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
-function runMiddleware(req: any, res: any, fn: any) {
+function runMiddleware(req: IncomingMessage, res: ServerResponse, fn: Function): Promise<void> {
     return new Promise((resolve, reject) => {
         fn(req, res, (result: any) => {
             if (result instanceof Error) {
@@ -17,10 +16,14 @@ function runMiddleware(req: any, res: any, fn: any) {
     });
 }
 
-async function handler(req: any, res: any) {
+async function handler(req: IncomingMessage, res: ServerResponse): Promise<void> {
     // Run the middleware
-    // @ts-ignore
     await runMiddleware(req, res, webhookCallback(bot, "std/http"));
+
+    // Send a response to acknowledge the request
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ status: 'ok' }));
 }
 
 export default handler;
